40a8d7a6286a233cd0648d7ddaf040e2e6fe4ec1
littlerA12 <105258386+littlerA12@users.noreply.github.com>
2022-08-09 22:40:33 -0300

    Add files via upload

---
+++ T4/imc.js
@@ -0,0 +1,6 @@
+let nome= 'Ana';
+let idade= 19;
+let altura= 1.56
+let peso= 54
+let imc= peso / (altura*altura)
+console.log('Ola ', nome, 'seu imc seria', imc, 'na sua idade:', idade, 'seu peso é', peso);
